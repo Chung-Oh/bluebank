984e3babbb7e676f954541ce4f30352290de39bf
Daniel Chung <danielvchung@hotmail.com>
2019-03-05 13:55:04 -0300

    public

--- src/Config/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-** Database configuration
-*/
-define('DB_DRIVER', 'mysql');
-define('HOSTNAME', 'localhost');
-define('DB_NAME', 'bluebank');
-define('USERNAME', 'root');
-define('PASSWORD', 'daniel');
